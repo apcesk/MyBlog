fdbe981af7ab740350408af07403117ac7d76fd5
张玉成 <zhangyucheng@zhangyuchengdeMacBook-Pro.local>
2020-03-26 17:49:27 +0800

    分类项目文件

---
+++ blog/components/Advert.js
@@ -0,0 +1,19 @@
+import '../static/style/components/Advert.css'
+
+const Advert = () => {
+
+    return (
+        // 广告区域
+        <div className="ad-div common-box">
+            <div>
+                {/* 广告图片 */}
+                <div><img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" /></div>
+                <div><img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" /></div>
+                <div><img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" /></div>
+                <div><img src="http://newimg.jspang.com/zhishixingqiu.png" width="100%" /></div>
+            </div>
+        </div>
+    )
+}
+
+export default Advert
