a66ded49373210c4331fdb6e5c1addb1076e3046
张玉成 <zhangyucheng@zhangyuchengdeMacBook-Pro.local>
2020-03-27 22:59:16 +0800

    一点修改

--- blog/pages/details.js
+++ blog/pages/details.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import {Col, Row, Breadcrumb, Affix} from 'antd'
+import axios from 'axios'
 import Header from '../components/Header'
 import {CalendarFilled,FolderFilled,FireFilled} from '@ant-design/icons'
 import Author from '../components/Author'
@@ -8,46 +9,39 @@ import Advert from '../components/Advert'
 import Footer from '../components/Footer'
 import '../static/style/pages/details.css'
 
-import ReactMarkdown from 'react-markdown'
 import MarkNav from 'markdown-navbar'
 import 'markdown-navbar/dist/navbar.css'
 
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai-sublime.css'
 
-const Details = () => {
-  let markdown='# P01:课程介绍和环境搭建\n' +
-  '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-  '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-   '**这是加粗的文字**\n\n' +
-  '*这是倾斜的文字*`\n\n' +
-  '***这是斜体加粗的文字***\n\n' +
-  '~~这是加删除线的文字~~ \n\n'+
-  '\`console.log(111)\` \n\n'+
-  '# p02:来个Hello World 初始Vue3.0\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n'+
-  '***\n\n\n' +
-  '# p03:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p04:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '#5 p05:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p06:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '# p07:Vue3.0基础知识讲解\n' +
-  '> aaaaaaaaa\n' +
-  '>> bbbbbbbbb\n' +
-  '>>> cccccccccc\n\n'+
-  '``` var a=11; ```';
+import Tocify from '../components/tocify.tsx'
+
+const Details = (props) => {
+  const tocify = new Tocify();
+  const renderer = new marked.Renderer();
+
+  renderer.heading = function(text, level, raw){
+    const anchor = tocify.add(text, level)
+    return `<a id="${anchor}" href="${anchor}" class="anchor-fix"><h${level}>${text}</h></a>`
+  }
+
+  marked.setOptions({
+    renderer:renderer,
+    gfm:true,//github format形式的代码
+    pedantic:false,//纠正markdown
+    sanitize:false,//是否忽略html标签
+    tables:true,
+    breaks:false,//是否可以有换行符
+    smartLists:true,//自动列表样式
+    highlight:function(code){
+      return hljs.highlightAuto(code).value
+    }
+  })
+
+  let html = marked(props.article_content);
+
   return (
     <div className="container">
       <Head>
@@ -73,11 +67,10 @@ const Details = () => {
                 <span><FolderFilled />视频教程 </span>
                 <span><FireFilled />1人 </span>
               </div>
-              <div className="detailed-content">
-                <ReactMarkdown 
-                  source={markdown}
-                  escapeHtml={false}
-                  />
+              <div className="detailed-content"
+                dangerouslySetInnerHTML={{__html:html}}
+              >
+                
               </div>
             </div>
           </div>
@@ -89,12 +82,7 @@ const Details = () => {
           <Affix offsetTop={5}>
             <div className="detailed-nav common-box">
               <div className="nav-title">文章目录</div>
-              <MarkNav
-                className="article menu"
-                source={markdown}
-                headingTopOffset={0} 
-                ordered={false}
-                />
+              {tocify && tocify.render()}
             </div>
           </Affix>
         </Col>
@@ -104,6 +92,21 @@ const Details = () => {
 
     </div>
   )
+}
+
+Details.getInitialProps = async (context)=>{
+  // console.log(context.query.id);
+  let id = context.query.id;
+  const promise = new Promise((resolve)=>{
+    axios('http://127.0.0.1:7001/default/getArticleById/'+id).then(
+      (res)=>{
+        // console.log(res);
+        resolve(res.data.data[0]);
+      }
+    )
+  })
+
+  return await promise;
 }
 
 export default Details
